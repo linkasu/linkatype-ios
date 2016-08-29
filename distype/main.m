7217658199238e0830bd27ab77dc76733ac273fd
Ivan <itruf@werbary.ru>
2016-08-29 19:43:17 +0300

    Initial commit

---
+++ distype/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  distype
+//
+//  Created by Иван Труфанов on 29.08.16.
+//  Copyright © 2016 aacidov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
