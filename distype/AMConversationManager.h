293ca1f0eee02a0799cf57764425cd8b7e7f5af6
Davarg <maka-dava@yandex.ru>
2016-09-14 18:13:05 +0300

    Raw implementation of ConversationManager

---
+++ distype/AMConversationManager.h
@@ -0,0 +1,22 @@
+//
+//  AMConversationManager.h
+//  distype
+//
+//  Created by amakushkin on 14.09.16.
+//  Copyright © 2016 aacidov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Realm/Realm.h>
+
+#import "AMConversationModel.h"
+
+@interface AMConversationManager : NSObject
+
++ (instancetype)sharedInstance;
+
+- (void)removeConversationWithUniqId:(NSString*)uniqId;
+- (NSArray<AMConversationModel*>*)getAllConversations;
+- (void)addNewConversationWithTitle:(NSString*)title;
+
+@end
