7555b6f2f5288a36446a507fc67521460412d046
Grigory S <grishamsc@gmail.com>
2017-12-22 01:12:56 +0300

    New sound of attracting attention
    Added alarm.wav
    Added Constants file
    Play new sound from MainCoordinator

---
+++ DisType/Source/Miscellaneous/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  DisType
+//
+//  Created by Grigory on 22/12/2017.
+//  Copyright © 2017 NixSolutions. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Sounds {
+        static let soundName = "alarm.wav"
+    }
+}
